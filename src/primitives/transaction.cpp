566347046e6e8d7f333f478042ce39730bed9e7e
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-04-23 09:58:36 +0800

    fix Formatmoney

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -53,7 +53,7 @@ CTxOut::CTxOut(const CAmount& nValueIn, CScript scriptPubKeyIn)
 
 std::string CTxOut::ToString() const
 {
-    return strprintf("CTxOut(nValue=%d.%06d, scriptPubKey=%s)", nValue / COIN, nValue % COIN, HexStr(scriptPubKey).substr(0, 30));
+    return strprintf("CTxOut(nValue=%d.%08d, scriptPubKey=%s)", nValue / COIN, nValue % COIN, HexStr(scriptPubKey).substr(0, 30));
 }
 
 CMutableTransaction::CMutableTransaction() : nVersion(CTransaction::CURRENT_VERSION), nTime(GetAdjustedTime()), nLockTime(0) {}
